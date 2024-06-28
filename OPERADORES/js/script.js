f0c9e5a6173bbe5793855421124d48151c5d8ea0
Rafael Avelar <81530504+rafaelavelar45@users.noreply.github.com>
2024-06-28 15:11:10 -0300

    Introdução ao JS com tipos de dados e operadores

---
+++ OPERADORES/js/script.js
@@ -0,0 +1,16 @@
+// 1 -  Tipo Number
+console.log(typeof 2);
+console.log(typeof 5.40)
+console.log(typeof -134)
+
+// 2 - Operação aritmetica
+
+
+console.log(1034 + 130)
+console.log(100 - 36)
+console.log(10 / 2)
+console.log(13 * 2)
+
+// Sempre respeitando a ordem de precencia
+
+console.log(5 + (8 * 2))
